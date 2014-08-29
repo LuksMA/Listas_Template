ad48f53d26fc4ccf588b578d167771b54f86c9f3
Harry Marquez <harry.marquez@ucsp.edu.pe>
2014-08-29 16:55:13 -0500

    0.2 Nodo next->next

--- Nodo.h
+++ Nodo.h
@@ -2,12 +2,13 @@
 #define NODO_H
 template <typename Type>
 class Node{
+public:
     Node *next;
-    Type info;
+    int info;
     Node(){
         next = 0;
     }
-    Node(Type i, Node * in = 0){
+    Node(int i, Node * in = 0){
         info = i;
         next = in;
     }
--- main.cpp
+++ main.cpp
@@ -7,8 +7,15 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    cout << "Hola mundo" << endl;
-    cout << " otro cambio" << endl;
+    Node<int> *p = new Node<int>(10);
+    p->next = new Node<int>(8);
+    p->next->next = new Node<int>(50);
+
+    cout << p->info <<endl;
+    cout << p->next->info << endl;
+    cout << p->next->next->info << endl;
+
+
 
     return a.exec();
 }
