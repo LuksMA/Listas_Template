b4a5f6030d7ead88898cb193d5b6976be994310f
Harry Marquez <harry.marquez@ucsp.edu.pe>
2014-08-29 16:21:38 -0500

    0.0

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <QCoreApplication>
+#include <iostream>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    cout << "Hola mundo" << endl;
 
     return a.exec();
 }
